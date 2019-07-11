e3a855ad8f769f4027cf41f9d449cf518909c3bc
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2019-07-11 22:53:47 +0530

    2.4.9

---
+++ JavaScript/sub-cat.js
@@ -0,0 +1,8 @@
+function showHideDiv() {
+    var div = document.getElementById("hideShow");
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
