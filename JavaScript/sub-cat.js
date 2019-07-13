3094317849ca0b9f0371cc8265ba8c833385c335
shelomi <50749107+shelomi99@users.noreply.github.com>
2019-07-13 15:50:01 +0530

    color changing dropdowns

--- JavaScript/sub-cat.js
+++ JavaScript/sub-cat.js
@@ -20,3 +20,26 @@ function hideNonVisibleDivs() {
         }
     }
 }
+
+
+function bgBlack() {
+    document.body.style.backgroundColor = "black";
+}
+
+function bgWhite() {
+    document.body.style.backgroundColor = "white";
+}
+function bgBlue() {
+    document.body.style.backgroundColor = "#22253c";
+}
+function txtBlack() {
+    document.body.style.color = "black";
+
+}
+function txtWhite() {
+    document.body.style.color = "white";
+}
+function txtBlue() {
+    document.body.style.color = "#22253c";
+}
+
