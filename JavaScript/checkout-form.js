7b52e9385ce286e3ceec5097b5d224dcb4d220b2
Nadin Kodithuwakku (NK) <nadin.2018430@iit.ac.lk>
2019-07-21 14:57:12 +0530

    checkout form with validation

--- JavaScript/checkout-form.js
+++ JavaScript/checkout-form.js
@@ -1,24 +1,37 @@
-function validate(){
-    var name=document.f1.name.value;
-    var passwordlength=document.f1.password.value.length;
+function validateForm() {
+    var first = document.forms["myForm"]["fname"].value;
+    var last = document.forms["myForm"]["lname"].value;
+    var x=document.forms["myForm"]["e-mail"].value;
+    var atposition=x.indexOf("@");
+    var dotposition=x.lastIndexOf(".");
     var status=false;
-    if(name==""){
-        document.getElementById("namelocation").innerHTML=
-            "Please enter your name";
+
+    //first name validation//
+    if (first == "") {
+        document.getElementById("fnamelocation").innerHTML="Please enter your first name !";
         status=false;
     }else{
-        //document.getElementById("namelocation").innerHTML=" <img src='http://www.javatpoint.com/javascriptpages/images/checked.gif'/>";
-        document.getElementById("namelocation").innerHTML="";
+        document.getElementById("fnamelocation").innerHTML="";
         status=true;
     }
-
-    if(passwordlength<6){
-        document.getElementById("passwordlocation").innerHTML=
-            "Password must be greater than 6";
+    //last name validation//
+    if (last == "") {
+        document.getElementById("lnamelocation").innerHTML="Please enter your last name !";
         status=false;
     }else{
-        //document.getElementById("passwordlocation").innerHTML=" <img src='http://www.javatpoint.com/javascriptpages/images/checked.gif'/>";
+        document.getElementById("lnamelocation").innerHTML="";
+        status=true;
     }
-
+    //email validation
+    if (x == "") {
+        document.getElementById("email_location").innerHTML = "Please enter your Email !";
+        status = false;
+    }else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
+        document.getElementById("email_location").innerHTML="Please enter a valid e-mail address !";
+        status= false;
+    }else{
+        document.getElementById("email_location").innerHTML = "";
+        
+    }
     return status;
 }
